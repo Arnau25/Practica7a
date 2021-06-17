24d8a09c3104c159aeffea09ee3f1fc5c47a9aaf
Arnau25 <81559427+Arnau25@users.noreply.github.com>
2021-06-17 12:26:48 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,36 @@
-#include <Arduino.h>
+#include "Arduino.h" 
+#include "FS.h"
+#include "HTTPClient.h"
+#include "SPIFFS.h"
+#include "SD.h"
+#include "AudioGeneratorAAC.h"
+#include "AudioOutputI2S.h"
+#include "AudioFileSourcePROGMEM.h"
+#include "sampleaac.h"
 
-void setup() {
-  // put your setup code here, to run once:
+
+
+AudioFileSourcePROGMEM *in;
+AudioGeneratorAAC *aac;
+AudioOutputI2S *out;
+
+void setup(){
+  Serial.begin(9600);
+
+  in = new AudioFileSourcePROGMEM(sampleaac, sizeof(sampleaac));
+  aac = new AudioGeneratorAAC();
+  out = new AudioOutputI2S();
+  out -> SetGain(0.125);
+  out -> SetPinout(26,25,22);
+  aac->begin(in, out);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  if (aac->isRunning()) {
+    aac->loop();
+  } else {
+    aac -> stop();
+    Serial.printf("Sound Generator\n");
+    delay(1000);
+  }
 }
